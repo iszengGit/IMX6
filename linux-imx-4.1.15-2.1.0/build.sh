3ebebac1f1170c28bae0a1077add9c58ef810bd5
zz <150469635@qq.com>
2022-08-03 16:50:56 +0800

    使用正点原子给的新内核源码之后，编译内核模块不再报错

--- linux-imx-4.1.15-2.1.0/build.sh
+++ linux-imx-4.1.15-2.1.0/build.sh
@@ -15,35 +15,41 @@ make distclean
 make imx_v7_defconfig -j 16
 #开始编译zImage
 make zImage -j 16
+#eMMC类型核心板
+#编译正点原子各种显示设备的设备树，若用户没有屏，
+#启动时默认会选择imx6ull-14x14-emmc-4.3-480x272-c.dtb加载
+make imx6ull-14x14-emmc-10.1-1280x800-c.dtb
+make imx6ull-14x14-emmc-7-1024x600-c.dtb
+make imx6ull-14x14-emmc-7-800x480-c.dtb
+make imx6ull-14x14-emmc-4.3-800x480-c.dtb
+make imx6ull-14x14-emmc-4.3-480x272-c.dtb
+make imx6ull-14x14-emmc-hdmi.dtb
+make imx6ull-14x14-emmc-vga.dtb
 
 #NandFlash类型核心板
 #编译正点原子各种显示设备的设备树，若用户没有屏，
 #启动时默认会选择imx6ull-14x14-nand-4.3-480x272-c.dtb加载
+make imx6ull-14x14-nand-10.1-1280x800-c.dtb
+make imx6ull-14x14-nand-7-1024x600-c.dtb
+make imx6ull-14x14-nand-7-800x480-c.dtb
+make imx6ull-14x14-nand-4.3-800x480-c.dtb
 make imx6ull-14x14-nand-4.3-480x272-c.dtb
-
+make imx6ull-14x14-nand-vga.dtb
+make imx6ull-14x14-nand-hdmi.dtb
 #编译内核模块
 make modules -j 16
-
 #在当前目录下新建一个tmp目录，用于存放编译后的目标文件
 if [ ! -e "./tmp" ]; then
     mkdir tmp
 fi
-
 rm -rf tmp/*
-
 make modules_install INSTALL_MOD_PATH=tmp
-
 cd tmp/lib/modules
-
 tar -jcvf ../../modules.tar.bz2 .
-
 cd -
-
 rm -rf tmp/lib
-
 #拷贝zImage到tmp目录下
 cp arch/arm/boot/zImage tmp
-
 #拷贝所有编译的设备树文件到当前的tmp目录下
 cp arch/arm/boot/dts/imx6ull*.dtb tmp
 echo "编译完成，请查看当前目录下的tmp文件夹查看编译好的目标文件"
